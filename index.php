89aa0ca5fe2cadadbb93ffe6e32484597ee1e1bd
Madhav Humagain <scimad2.71@gmail.com>
2016-08-02 02:28:41 +0545

    Initial Commit for PHP-SDK

--- index.php
+++ index.php
@@ -1,54 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>FB-API test</title>
-</head>
-<body>
-<script id="scimad00">
-    window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '384590035054335',
-        xfbml      : true,
-        version    : 'v2.7'
-    });
-
-    function onLogin(response) {
-        console.log("I am logging a variable");
-        console.log(response);
-        if (response.status == 'connected') {
-            FB.api('/me?fields=first_name', function(data) {
-                var welcomeBlock = document.getElementById('fb-welcome');
-                welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-            });
-        }
-    }
-
-    FB.getLoginStatus(function(response) {
-        // Check login status on load, and if the user is
-        // already logged in, go directly to the welcome message.
-        if (response.status == 'connected') {
-            onLogin(response);
-        } else {
-            // Otherwise, show Login dialog first.
-            FB.login(function(response) {
-            onLogin(response);
-            }, {scope: 'user_friends, email'});
-        }
-    });
-
-    };
-    (function(d, s, id){
-        console.log("I am working 00");
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-<h1 id="fb-welcome">Welcome</h1>
+<?php
+    require_once './vendor/autoload.php';
+    $fb = new Facebook\Facebook([
+        'app_id' => '384590035054335',
+        'app_secret' => 'f1469729370c9cb96d8db41e9e540a1a',
+        'default_graph_version' => 'v2.5',
+    ]);
 
 
-</body>
-</html>
+    //$fb = new Facebook\Facebook([/* . . . */]);
+
+    $helper = $fb->getRedirectLoginHelper();
+    $permissions = ['email', 'user_likes']; // optional
+    $loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
+
+    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>'; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
