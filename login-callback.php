ecb4953791844f51837468054f69cc139f11a14e
Madhav Humagain <scimad2.71@gmail.com>
2016-08-02 02:37:11 +0545

    modified login-url

---
+++ login-callback.php
@@ -0,0 +1,33 @@
+<?php
+    require_once './vendor/autoload.php';
+    $fb = new Facebook\Facebook([
+        'app_id' => '384590035054335',
+        'app_secret' => 'f1469729370c9cb96d8db41e9e540a1a',
+        'default_graph_version' => 'v2.5',
+    ]);
+
+
+    echo "string";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
