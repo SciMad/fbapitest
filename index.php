b91bfd7f0be1b67b4a9c565a140dd1c69f7ab89c
Madhav Humagain <scimad2.71@gmail.com>
2016-08-02 01:51:17 +0545

    don't know :D

--- index.php
+++ index.php
@@ -11,7 +11,31 @@
         xfbml      : true,
         version    : 'v2.7'
     });
-    // ADD ADDITIONAL FACEBOOK CODE HERE
+
+    function onLogin(response) {
+        console.log("I am logging a variable");
+        console.log(response);
+        if (response.status == 'connected') {
+            FB.api('/me?fields=first_name', function(data) {
+                var welcomeBlock = document.getElementById('fb-welcome');
+                welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+            });
+        }
+    }
+
+    FB.getLoginStatus(function(response) {
+        // Check login status on load, and if the user is
+        // already logged in, go directly to the welcome message.
+        if (response.status == 'connected') {
+            onLogin(response);
+        } else {
+            // Otherwise, show Login dialog first.
+            FB.login(function(response) {
+            onLogin(response);
+            }, {scope: 'user_friends, email'});
+        }
+    });
+
     };
     (function(d, s, id){
         console.log("I am working 00");
@@ -24,36 +48,6 @@
 </script>
 
 <h1 id="fb-welcome">Welcome</h1>
-
-<script type="text/javascript">
-    
-function onLogin(response) {
-    console.log(response);
-    if (response.status == 'connected') {
-        FB.api('/me?fields=first_name', function(data) {
-            var welcomeBlock = document.getElementById('fb-welcome');
-            welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-        });
-    }
-}
-
-
-
-FB.getLoginStatus(function(response) {
-    // Check login status on load, and if the user is
-    // already logged in, go directly to the welcome message.
-    if (response.status == 'connected') {
-        onLogin(response);
-    } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
-        onLogin(response);
-    }, {scope: 'user_friends, email'});
-}
-});
-
-</script>
-
 
 
 </body>
