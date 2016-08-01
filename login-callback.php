bd9e5737cafca6aa65ade5496aab32b9c667d0fc
Madhav Humagain <scimad2.71@gmail.com>
2016-08-02 02:46:51 +0545

    minor edit

--- login-callback.php
+++ login-callback.php
@@ -1,33 +1,28 @@
 <?php
     require_once './vendor/autoload.php';
+    
     $fb = new Facebook\Facebook([
         'app_id' => '384590035054335',
         'app_secret' => 'f1469729370c9cb96d8db41e9e540a1a',
         'default_graph_version' => 'v2.5',
     ]);
 
+    $helper = $fb->getJavaScriptHelper();
+    try {
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
 
-    echo "string";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+if (isset($accessToken)) {
+  // Logged in
+    echo "Authorised";
+}else{
+    echo "Not Authorised";
+}
